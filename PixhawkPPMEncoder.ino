4cb29101d9c24dc5a38681aee225d01e20305aa4
Unknown <leon.jungemeyer@me.com>
2019-04-15 15:25:06 +0200

    Added PPM Out

--- PixhawkPPMEncoder.ino
+++ PixhawkPPMEncoder.ino
@@ -1,10 +1,31 @@
+
+//CONFIG
 #define INPUT_PIN 2  //Interrupt supporting PINs only
+#define OUTPUT_PIN 8
 #define NUM_CHANNELS 6
 
+//TUNIG PARAMS
 #define CHANNEL_WIDTH 15 //Maximum amount of channels PPM supports, should not be changed
+#define ON_STATE 1  //set polarity of the pulses: 1 is positive, 0 is negative
+#define DEFAULT_SERVO_VALUE 1500  //set the default servo value
+#define PPM_FRAME_LENGTH 22500  //set the PPM frame length in microseconds (1ms = 1000µs)
+#define PPM_PULSE_LENGTH 300  //set the pulse length
 
-unsigned long int time_falling, last_peak, pulse_time;
+unsigned long int last_peak;
 int pulse_buffer[CHANNEL_WIDTH], channel_buffer[CHANNEL_WIDTH], current_channel;
+int out_channels[NUM_CHANNELS];
+
+// Setup PPM OUT Registers
+void ppm_out_registers() {
+    cli();
+    TCCR1A = 0; // set entire TCCR1 register to 0
+    TCCR1B = 0;
+    OCR1A = 100;  // compare match register, change this
+    TCCR1B |= (1 << WGM12);  // turn on CTC mode
+    TCCR1B |= (1 << CS11);  // 8 prescaler: 0,5 microseconds at 16mhz
+    TIMSK1 |= (1 << OCIE1A); // enable timer compare interrupt
+    sei();
+}
 
 void setup() {
     Serial.begin(9600);
@@ -13,13 +34,22 @@ void setup() {
     pinMode(INPUT_PIN, INPUT_PULLUP);
     current_channel = 0;
     attachInterrupt(digitalPinToInterrupt(INPUT_PIN), read_me, FALLING);
+
+    //Setup PPM Write
+    for(int i = 0; i < NUM_CHANNELS; i++){
+        out_channels[i] = DEFAULT_SERVO_VALUE;
+    }
+    pinMode(OUTPUT_PIN, OUTPUT);
+    digitalWrite(OUTPUT_PIN, !ON_STATE); 
+    
+    ppm_out_registers();
 }
 
 void read_me() {
 
     //Calculate peak time
-    time_falling = micros();
-    pulse_time = time_falling - last_peak; 
+    unsigned long int time_falling = micros();
+    unsigned long int pulse_time = time_falling - last_peak; 
     last_peak = time_falling;
 
     // Sture current poulse into array
@@ -52,7 +82,7 @@ int *read_rc() {
     }
 
     return ch;
-} 
+}
 
 void loop() {
     int *ch = read_rc();
@@ -60,8 +90,41 @@ void loop() {
     for(int i=0; i<NUM_CHANNELS;i++) {
       Serial.print(ch[i]);
       Serial.print(", ");
+      out_channels[i] =  ch[i];
     }
     Serial.print("\n");
-    delay(100);
+    delay(10);
+}
+
+
+ISR(TIMER1_COMPA_vect){
+  static boolean state = true;
+  
+  TCNT1 = 0;
+  
+  if(state) {  //start pulse
+    digitalWrite(OUTPUT_PIN, ON_STATE);
+    OCR1A = PPM_PULSE_LENGTH * 2;
+    state = false;
+  }
+  else{  //end pulse and calculate when to start the next pulse
+    static byte cur_chan_numb;
+    static unsigned int calc_rest;
+  
+    digitalWrite(OUTPUT_PIN, !ON_STATE);
+    state = true;
+
+    if(cur_chan_numb >= NUM_CHANNELS){
+      cur_chan_numb = 0;
+      calc_rest = calc_rest + PPM_PULSE_LENGTH;
+      OCR1A = (PPM_FRAME_LENGTH - calc_rest) * 2;
+      calc_rest = 0;
+    }
+    else{
+      OCR1A = (out_channels[cur_chan_numb] - PPM_PULSE_LENGTH) * 2;
+      calc_rest = calc_rest + out_channels[cur_chan_numb];
+      cur_chan_numb++;
+    }     
+  }
 }
 
